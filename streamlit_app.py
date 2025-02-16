096e5cd0f5bcb6e25530056f5d3a0ec02e315cfb
pipertec <pipertecmaster@gmail.com>
2025-02-16 15:16:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import random
 import time
 from google import genai
 from google.genai import types
+client = genai.Client(api_key="AIzaSyBR4x9HaeWtdkD3u-rqLE47Mb570nOsE_I")
+
+
+
 
 st.title("Advantage Software Expert")
 
@@ -27,6 +31,6 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
      
       response = client.models.generate_content(model="gemini-2.0-flash-lite-preview-02-05",contents=[prompt],config=types.GenerateContentConfig(max_output_tokens=1000,temperature=0.1,))
-st.write(response.text)
+      st.write(response.text)
 
     st.session_state.messages.append({"role": "assistant", "content": response})
