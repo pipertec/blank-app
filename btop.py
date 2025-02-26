c69e5f86e0946671226153733f97c43a83246f37
pipertec <pipertecmaster@gmail.com>
2025-02-26 13:45:56 -0500

    Update btop.py

--- btop.py
+++ btop.py
@@ -125,7 +125,7 @@ if prompt := st.chat_input("What is your Advantage Software question or comment?
      
      
       # response = client.models.generate_content(model="gemini-2.0-flash-lite-preview-02-05",contents=[prompt],config=types.GenerateContentConfig(max_output_tokens=1000,temperature=0.1,system_instruction=raw_text,))
-      response = client.models.generate_content(model="gemini-2.0-flash",contents=[prompt],config=types.GenerateContentConfig(max_output_tokens=1000,temperature=0.1,system_instruction=raw_text,))
+      response = client.models.generate_content(model="gemini-2.0-flash",contents=[prompt],config=types.GenerateContentConfig(max_output_tokens=3000,temperature=0.1,system_instruction=raw_text,))
 
       st.write(response.text)
 
